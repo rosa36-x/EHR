15041ea24289c3a10ebf97edded9f0745eefdc5f
Rosalin <rosalinv36@gmail.com>
2026-06-22 10:30:43 +0530

    Added CreateReferral function

--- chaincode/models/referral.go
+++ chaincode/models/referral.go
@@ -1,1 +1,14 @@
 package models
+
+type Referral struct {
+	ReferralID       string `json:"referralID"`
+	PatientID        string `json:"patientID"`
+	FromDoctorID     string `json:"fromDoctorID"`
+	ToDoctorID       string `json:"toDoctorID"`
+	ReferralCID      string `json:"referralCID"`
+	ReferralHash     string `json:"referralHash"`
+	EncryptionKeyRef string `json:"encryptionKeyRef"`
+	ReferralReason   string `json:"referralReason"`
+	CreatedAt        string `json:"createdAt"`
+	UpdatedAt        string `json:"updatedAt"`
+}
--- chaincode/smartcontracts.go
+++ chaincode/smartcontracts.go
@@ -259,3 +259,46 @@ func (s *SmartContract) RevokeConsent(
 		consentJSON,
 	)
 }
+func (s *SmartContract) CreateReferral(
+	ctx contractapi.TransactionContextInterface,
+	referralID string,
+	patientID string,
+	fromDoctorID string,
+	toDoctorID string,
+	referralCID string,
+	referralHash string,
+	encryptionKeyRef string,
+	referralReason string,
+	createdAt string,
+	updatedAt string,
+) error {
+
+	exists, err := ctx.GetStub().GetState(referralID)
+	if err != nil {
+		return err
+	}
+
+	if exists != nil {
+		return fmt.Errorf("referral %s already exists", referralID)
+	}
+
+	referral := models.Referral{
+		ReferralID:       referralID,
+		PatientID:        patientID,
+		FromDoctorID:     fromDoctorID,
+		ToDoctorID:       toDoctorID,
+		ReferralCID:      referralCID,
+		ReferralHash:     referralHash,
+		EncryptionKeyRef: encryptionKeyRef,
+		ReferralReason:   referralReason,
+		CreatedAt:        createdAt,
+		UpdatedAt:        updatedAt,
+	}
+
+	referralJSON, err := json.Marshal(referral)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(referralID, referralJSON)
+}
